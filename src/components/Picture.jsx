2c39783991010f0b6cff5a98a0607b8d0be31598
buraktr03 <turanburak@gmail.com>
2023-07-08 14:55:29 +0300

    last edit

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -2,8 +2,10 @@ import "../styles/style.css";
 
 const Picture = ({ photographer, src }) => {
   return (
-    <div className="card text-center p-2 m-3 border-0">
-      <img src={src.large} className="card-img-top rounded-4" alt="..." />
+    <div className="card text-center border-0">
+      <div className="img-container">
+        <img src={src.large} className="card-img-top rounded-4" alt="..." />
+      </div>
       <div className="card-body p-2">
         <h5 className="card-title">{photographer}</h5>
       </div>
